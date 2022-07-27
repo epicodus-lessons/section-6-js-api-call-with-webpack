6a0f2be3e42b77fb002c91f621e8bfbfb239fcbf
Brooke Kullberg <brookekullberg@gmail.com>
2022-07-26 21:06:40 -0700

    add working project

---
+++ src/index.js
@@ -0,0 +1,38 @@
+import 'bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './css/styles.css';
+import WeatherService from './weather-service.js';
+
+// Business Logic
+
+async function getWeather(city) {
+  const response = await WeatherService.getWeather(city);
+  if (response.main) {
+    printElements(response, city);
+  } else {
+    printError(response, city);
+  }
+}
+
+// UI Logic
+
+function printElements(response, city) {
+  document.querySelector('#showResponse').innerText = `The humidity in ${city} is ${response.main.humidity}%.
+  The temperature in Kelvins is ${response.main.temp} degrees.`;
+}
+
+function printError(error, city) {
+  document.querySelector('#showResponse').innerText = `There was an error accessing the weather data for ${city}: 
+  ${error}.`;
+}
+
+function handleFormSubmission(event) {
+  event.preventDefault();
+  const city = document.querySelector('#location').value;
+  document.querySelector('#location').value = null;
+  getWeather(city);
+}
+
+window.addEventListener("load", function() {
+  document.querySelector('form').addEventListener("submit", handleFormSubmission);
+});
